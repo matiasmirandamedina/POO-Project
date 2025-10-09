64871bc9c39ba7cc04d197892f48648fe7db7fff
MarceloAlanoca <elfalopade2024@gmail.com>
2025-10-09 16:41:06 -0300

    Base del proyecto.

--- mini_home_banking/Vistas/Form1.Designer.cs
+++ mini_home_banking/Vistas/Form1.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace practica
+﻿namespace mini_home_banking.Vistas
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,8 +23,8 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            button1.Location = new Point(260, 189);
+            button1.Location = new Point(75, 26);
             button1.Name = "button1";
-            button1.Size = new Size(374, 108);
+            button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
-            button1.Text = "conexión";
+            button1.Text = "Conectar";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
@@ -45,7 +45,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(219, 77);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
